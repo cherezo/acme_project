7575509eb6d5df8f112a16c9651c6346999dff10
Roman Mazovetskiy <rommaz@yandex.ru>
2023-05-31 18:56:50 +0500

    Sprint 7/18 → Theme 3/8: → Lesson 2/4 Ready

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,5 +1,45 @@
 from django import forms
+# Импортируем класс модели Birthday.
+from .models import Birthday
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
 
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
+
+#Создание формы на основе модели
+class BirthdayForm(forms.ModelForm):
+
+    # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        #Не забываем указание требуемых виджетов (например для поля 'birthday')
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
+
+#Создание самостоятельной формы
+'''
 class BirthdayForm(forms.Form):
     first_name = forms.CharField(
         label='Имя',
@@ -14,3 +54,4 @@ class BirthdayForm(forms.Form):
         label='Дата рождения',
         widget=forms.DateInput(attrs={'type': 'date'})    
     )
+'''
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,29 @@
 from django.db import models
+# Импортируется функция-валидатор.
+from .validators import real_age
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+    )
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+
+'''Добавим к модели Birthday проверку на уникальность записи: совокупность значений полей
+ «Имя», «Фамилия» и «Дата рождения» не должна повторяться в БД. 
+Подобные проверки настраиваются с помощью атрибута constraints (англ. «ограничения») подкласса Meta,
+ где указывается класс model.UniqueConstraint (ограничение на уникальность). 
+В этом классе указывается:
+- перечень полей, совокупность которых должна быть уникальна;
+- имя ограничения.
+'''
+class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
